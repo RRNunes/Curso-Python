0143c61355d91331017374b9fc7d95a3e79600d0
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 05:04:52 +0000

    d10c

--- Exercicios/Desafios/des10.py
+++ Exercicios/Desafios/des10.py
@@ -1,4 +1,5 @@
 # Crie um programa que leia quanto a pessoa tem na carteira e mostre quantos dolares ela pode comprar, CONSIDERANDO O DOLAR $3,57
-real = float(input('Digite quanto diheiro você tem na cateira: ')) 
+real = float(input('Digite quanto diheiro você tem na cateira: R$')) 
 dolar = real * 3.27
-print('Com R${} você pode comprar ${} dolares.'.format(real, dolar)) 
+print('Com R${:.2f} você pode comprar ${:.2f} dolares.'.format(real, dolar)) 
+
