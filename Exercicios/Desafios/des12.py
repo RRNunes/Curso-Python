cc5e016476009bd694e89b902b050eaee410331a
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 05:17:09 +0000

    d12c

--- Exercicios/Desafios/des12.py
+++ Exercicios/Desafios/des12.py
@@ -1,4 +1,4 @@
 # Faça um algoritimo que leia o prec2o de um produto e mostre o novo prec2o com 5% de desconto
-p = float(input('Qual o preço do produto? '))
+p = float(input('Qual o preço do produto? R$'))
 n = p - (p * 5 / 100)
-print('O prec1o com deconto é R${}'.format(n))
+print('O prec1o com deconto é R${:.2f}'.format(n))
