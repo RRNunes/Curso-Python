9b746cd8158735269d81f80792397e22f6f7dae6
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 03:09:10 +0000

    des7

---
+++ Exercicios/Desafios/des07.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as duas notas de um aluno e calcule e mostre sua media.
+n1 = int(input('Digite sua primeira nota: ')) 
+n2 = int(input('Digite sua segunda nota: ')) 
+m = (n1 + n2) / 2
+print('Sua nota média é {}.'.format(m)) 
