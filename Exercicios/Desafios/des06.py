3998be6d34032a207a8d4b20ee708af8ce4ed0a2
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 02:49:44 +0000

    d6

---
+++ Exercicios/Desafios/des06.py
@@ -0,0 +1,7 @@
+#Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n = int(input('Digite um valor: ')) 
+d = n * 2
+t = n * 3
+q = n ** (1/2)
+print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {}'.format(n, d, t, q)) 
+
