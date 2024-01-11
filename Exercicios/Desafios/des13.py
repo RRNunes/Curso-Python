dcfeda9167e0743595df667f06abe7759838c80a
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 05:21:49 +0000

    d12c

--- Exercicios/Desafios/des13.py
+++ Exercicios/Desafios/des13.py
@@ -1,4 +1,4 @@
 # Fac1a um algoritimo que calcule o almento de 15% no salario dos funcionarios
-s = float(input('Digite salario: '))
+s = float(input('Digite salario: R$'))
 ns = s + (s * 15 / 100)
-print('O novo salario é R${}'.format(ns)) 
+print('O novo salario é R${:.2f}'.format(ns)) 
