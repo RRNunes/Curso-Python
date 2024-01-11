48d11e9d7cc596c86619147196817927f9347f16
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 04:34:18 +0000

    d5c

--- Exercicios/Desafios/des05.py
+++ Exercicios/Desafios/des05.py
@@ -1,5 +1,3 @@
 #Faça um programa que leia um número inteiro o seu sucessor e antecessor
 n = int(input('Digite um número: '))
-a = n - 1
-s = n + 1
-print('O antecessor do número {} é {} e o sucessor é {}'.format(n, a, s)) 
+print('O antecessor do número {} é {} e o sucessor é {}'.format(n, (n-1), (n+1))) 
