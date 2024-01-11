19a998bd468a29ceaa48d8a8115539f81f778dbf
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 03:18:26 +0000

    d8

---
+++ Exercicios/Desafios/des08.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um valor em metros e o exbiba convertido em centimetros e milimetros.
+v = float(input('Digite medida: ')) 
+cm = v * 100
+mm = v *1000
+print('A medida é {}cm \n A medida é {}mm'.format(cm, mm))
+
