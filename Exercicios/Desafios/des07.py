b9a532f67530f4a6a6ca1b56f420fd36550d08b4
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 04:51:23 +0000

    d7c

--- Exercicios/Desafios/des07.py
+++ Exercicios/Desafios/des07.py
@@ -2,4 +2,4 @@
 n1 = int(input('Digite sua primeira nota: ')) 
 n2 = int(input('Digite sua segunda nota: ')) 
 m = (n1 + n2) / 2
-print('Sua nota média é {}.'.format(m)) 
+print('Sua nota média é {:.1f}.'.format(m)) 
