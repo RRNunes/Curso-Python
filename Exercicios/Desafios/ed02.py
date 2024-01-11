98f8540cca945f8547b90c241a7c167cf8aadc8e
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-01-10 19:16:20 -0500

    v2

--- Exercicios/Desafios/ed02.py
+++ Exercicios/Desafios/ed02.py
@@ -1,6 +1,6 @@
 #Crie um script Python que leia o dia, o mes e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
 
-d = int(input('Que dia voce nasceu?'))
-m = input('Qual o mes que voce nasceu?')
+d = int(input('Que dia você nasceu?'))
+m = input('Qual o mes que você nasceu?')
 a = int(input('Qual seu ano de nascimento?'))
-print('Voce nasceu no dia {} de {} de {}. Correto?'.format(d, m, a)) 
+print('Você nasceu no dia {} de {} de {}. Correto?'.format(d, m, a)) 
