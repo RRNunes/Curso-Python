9dd2df1ab46fe5da6a65d6c4e218a0ae5f2f19a9
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-11 03:43:35 +0000

    d9

---
+++ Exercicios/Desafios/des09.py
@@ -0,0 +1,23 @@
+# Crie um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+n = int(input('Digite um número')) 
+t1 = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+print('a tabuada do número {} é...'.format(n)) 
+print('{} x 1 = {}'.format(n, t1))
+print('{} x 2 = {}'.format(n, t2)) 
+print('{} x 3 = {}'.format(n, t3))
+print('{} x 4 = {}'.format(n, t4)) 
+print('{} x 5 = {}'.format(n, t5))
+print('{} x 6 = {}'.format(n, t6))
+print('{} x 7 = {}'.format(n, t7))
+print('{} x 8 = {}'.format(n, t8))
+print('{} x 9 = {}'.format(n, t9))
+print('{} x 10 = {}'.format(n, t10))
