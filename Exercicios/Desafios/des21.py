ad647c4273259eb7f484b97f4a25e9b5dd22a8b6
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-01-11 21:32:09 -0500

    d21

--- Exercicios/Desafios/des21.py
+++ Exercicios/Desafios/des21.py
@@ -1,3 +1,7 @@
 # Faca um programa em Python que abra e reproduza o audio de um arquivo mp3.
 
 import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.play()
+pygame.event.wait
