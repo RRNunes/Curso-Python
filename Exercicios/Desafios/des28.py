55284ad69c7f79e02d81a96f4abfb27e7512f120
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-13 22:55:22 +0000

    d29

--- Exercicios/Desafios/des28.py
+++ Exercicios/Desafios/des28.py
@@ -1,3 +1,16 @@
 # Escreva um programa que faca o computador 'pensar'em um numero inteiro entre 0 e 5 e peca para o usuario. tentar dsscobrir qual foi o numero escolhido pelo computador
 # o programa devera escrecer na tela se o usuario venceu ou perdeu.
 
+from random import randint
+from time import sleep
+computador = randint(0, 5)
+print('Vou pensar em um número de 0 a 5 tente adivinhar...')
+jogador = int(input('Em que numero pensei?'))
+print('Processando...')
+sleep(3)
+if jogador == computador:
+    print('PARABÉNS! Voce acertou o numero!')
+else:
+    print('Voce perdeu!!!!O numero que pensei foi {} e nao {}. '.format(computador, jogador))
+
+    
--- Exercicios/Desafios/des29.py
+++ Exercicios/Desafios/des29.py
@@ -1,3 +1,10 @@
 #Escreva um programa que leia a velocidade de um carro. 
+velocidade_carro = float(input('Digite a velocidade do carro: '))
 #se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
+if velocidade_carro > 80:
+    print('MULTADO! Voce ultrapassou o limite de velocidade de 80km/h.')
 #A multa vai custar R$7,00 por km acima do limite
+    multa = (velocidade_carro -80) * 7
+    print('Sua multa é de R${}.'.format(multa))
+print('Tenha um bom dia! Dirija com segurancao!')
+
