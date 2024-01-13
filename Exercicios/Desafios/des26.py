2aa18ffd6981028f2880a37f9af22e7b499b07e3
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-13 04:24:43 +0000

    d27c

--- Exercicios/Desafios/des26.py
+++ Exercicios/Desafios/des26.py
@@ -3,7 +3,7 @@
 #Em que posicao ela aparece a primeira vez
 # Em que posicao aparece pela ultima vez
 
-frase = input('Digite uma frase: ').upper()
+frase = input('Digite uma frase: ').upper().strip()
 print('Quantas vezes aparece a letra A na frase? {}'.format(frase.count('A')))
-print('A primeira letra A aparece na posicao {}'.format(frase.find('A')))
-print('A ultima letra A aparece na possicao {}'.format(frase.rfind('A')))
+print('A primeira letra A aparece na posicao {}'.format(frase.find('A')+1))
+print('A ultima letra A aparece na possicao {}'.format(frase.rfind('A')+1))
--- Exercicios/Desafios/des27.py
+++ Exercicios/Desafios/des27.py
@@ -1,6 +1,6 @@
 # Faca um programa que leia o nome de uma pessoa. mostre em seguida o primeiro e o ultimo nome separadamente.
 
-nome_completo = input('Qual seu nome completo? ')
+nome_completo = input('Qual seu nome completo? ').strip()
 nome = nome_completo.split()
 print('O seu primeiro nome é {}'.format(nome[0]))
 print('Seu ultimo nome é? {}'.format(nome[len(nome) - 1]))
