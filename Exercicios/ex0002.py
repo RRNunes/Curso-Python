80ac745dc0f3802f0a64cd61651c2580bb0ef674
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-15 23:50:40 +0000

    d36

--- Exercicios/ex0002.py
+++ Exercicios/ex0002.py
@@ -1,2 +1,3 @@
 n = input("Olá! Digite seu nome: ")
 print('É um prazer te conhecer, {}!'.format(n))
+
---
+++ Mundo 2/des36.py
@@ -0,0 +1,15 @@
+"""Escreva um programa para aprovar o emprestimo bancario para  a compra de uma casa.
+O programa vai perguntar o valor da casa, o salario do comprador e em quantos anos ele vai pagar. 
+Calcule o valor da prestacao mensal, sabendo que ela nao pode exceder 30% do salario ou entao o emprestimo sera negado"""
+casa = float(input('Qual o valor da casa? R$'))
+salario = float(input('Qual o salario do comprador? R$'))
+ano = int(input('Em quantos anos deseja pagar? '))
+prestacao = casa / ano
+if prestacao > (salario *30//100):
+    print('Seu emprestino nao foi aprovado!')
+elif prestacao <= (salario *30//100):
+    print('Seu emprestimo foi aprovado!')
+else:
+    total = prestacao + salario
+    print('Seu financiamento tera o valor total de R${}. Parabens!'.format(total))
+    
