c9c9cd50d30f893dc7b7f1fed9a13f3b3058d760
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-16 01:04:10 +0000

    d36c

--- Mundo 2/des36.py
+++ Mundo 2/des36.py
@@ -1,15 +1,16 @@
 """Escreva um programa para aprovar o emprestimo bancario para  a compra de uma casa.
 O programa vai perguntar o valor da casa, o salario do comprador e em quantos anos ele vai pagar. 
 Calcule o valor da prestacao mensal, sabendo que ela nao pode exceder 30% do salario ou entao o emprestimo sera negado"""
+
 casa = float(input('Qual o valor da casa? R$'))
 salario = float(input('Qual o salario do comprador? R$'))
 ano = int(input('Em quantos anos deseja pagar? '))
-prestacao = casa / ano
-if prestacao > salario + (salario *30//100):
-    print('Seu emprestino nao foi aprovado!')
-elif prestacao <= salario + (salario *30//100):
-    print('Seu emprestimo foi aprovado!')
+prestacao = casa / (ano * 12)
+minimo = salario *30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, ano))
+print('A prestacao sera de R4{:.2f}'.format(prestacao))
+if prestacao <= salario:
+    print('Emprestimo APROVADO!!!')
 else:
-    total = prestacao + salario
-    print('Seu financiamento tera o valor total de R${}. Parabens!'.format(total))
+    print('Emprestimo NEGADO!!!')
     
