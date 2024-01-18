f05984bc75efa0822d9fad4346af655f323fadad
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-18 23:08:51 +0000

    d45c

--- Mundo 2/des45.py
+++ Mundo 2/des45.py
@@ -1,1 +1,49 @@
 """Çrie um programa que faca o computador jogar Jokenpo com voce"""
+
+from random import randint
+from time import sleep
+itens = ('Pedra', ['Papel'], ['Tesoura'])
+computador = randint(0, 2)
+print(''' Suas opcoes
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO')
+sleep(1)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format([jogador]))
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('jogador venceu')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU')
+    else:
+        print('JOGADA INVALIDA')
+
+if computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU')
+    else:
+        print('JOGADA INVALIDA')
+
+if computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCEU')
+    elif jogador == 1:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
+
