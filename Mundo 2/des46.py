89598d7f4b5ea5cf316bc9d345fb630bb7b0bb76
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-20 01:26:21 +0000

    des48c

--- Mundo 2/des46.py
+++ Mundo 2/des46.py
@@ -2,7 +2,7 @@
 indo de 10 ate 0, com uma pausa de um segundo"""
 
 from time import sleep
-for c in range(10, -1, -1):
-   print(c)
+for cont in range(10, -1, -1):
+   print(cont)
    sleep(1)
 print('PARAPAPA POWWWW!')
--- Mundo 2/des47.py
+++ Mundo 2/des47.py
@@ -1,5 +1,5 @@
 """ Crie um programa que mostre na tela todos os numeros pares que estao no intervalo entre 1 e 50"""
 
-for c in range(0, 50, 2):
-    print(c)
+for c in range(2, 50, 2):
+    print(c, end=' ')
 print('FIM!')
