8ab9bf5b3b221ec956ddfb304090328aa0ff9809
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-20 00:01:50 +0000

    ex13

---
+++ Exercicios/ex13.py
@@ -0,0 +1,15 @@
+# Lacos
+
+# for c in range(1,10):
+    #PASSO
+#PEGA
+
+
+#for c in range(0, 7, 2):
+#
+    print(c)
+#print('FIM!')
+
+n = int(input('Digite um numero: '))
+for c in range(0, n):
+    print(c)
