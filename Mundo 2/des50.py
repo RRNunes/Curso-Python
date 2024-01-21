39267d2b2b99defb7363afa43bfa7ea4fd61a413
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-21 00:53:45 +0000

    d50

--- Mundo 2/des50.py
+++ Mundo 2/des50.py
@@ -1,5 +1,11 @@
 """"DESENVOLVA UM PROGRAMA QUE LEIA E MOSTRE A SOMA APENAS DAQUELES QUE FOREM PARES. SE O VALOR DIGITADO
 FOR IMPAR, DESCONCIDERE-O"""
 
-n = int(input('Digite numero: '))
-for c in range()
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Digite o {} valor: '.format(c)))
+    if n % 2 == 0:
+        soma += n
+        cont += 1 
+print('Voce informou {} numero e a soma foi {}'.format(cont, soma))
