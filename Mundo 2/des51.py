8f9f83fee60de408a75b1b5d5a401d3d0ca3e1dd
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-21 01:09:57 +0000

    d51c

--- Mundo 2/des51.py
+++ Mundo 2/des51.py
@@ -1,1 +1,8 @@
 """"DESENVOLVA UM PROGRAMA QUE LEIA O PRIMEIRO TERMO E A RAZAO DE UMA pa. nO FINAL, MOSTRE OS 10 PRIMEIROS TERMOS DESSA PROGRESSAO"""
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo, razao):
+    print('{}'.format(c), end=' ')
+print('ACABOU!')
