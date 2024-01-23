4ef73ca6b9e0e4ee0340d7a0f8a779d53a8beda7
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-23 21:43:45 +0000

    d54c

--- Mundo 2/des52.py
+++ Mundo 2/des52.py
@@ -1,4 +1,17 @@
 """""FACE UM PROGRAMA QUE LEIA UM NU ERO INTEIRO E DIGA SE ELE É OU NAO UM NUMERO PRIMO."""
 
-print('Digite um numero: ')
-
+numero = int(input('Dige um valor: '))
+tot = 0
+for c in range(1, numero + 1 ):
+    if numero % c == 0:
+        print('\033[33m', end=' ')
+        tot += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{}'.format(c), end=' ')
+print('\n\033[mO numero {} foi divisivel {} vezes'.format(numero, tot))
+if tot == 2:
+    print('E por isso É PRIMO')
+else:
+    print('E por isso ele NAO É PRIMO')
+    
--- Mundo 2/des54.py
+++ Mundo 2/des54.py
@@ -1,2 +1,16 @@
 """"CRIE UM PROGRAMA QUE LEIA O ANO DE NASCIMENTO DE SETE PESSOAS. NO FINAL, MOSTRE QUANTAS PESSOAS
 AINDA NAO ATINGIRAM A MAIORIDADE E QUANTAS SAO AMIORES"""
+
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range(1, 8):
+    nascimento = int(input('Em que ano a {} pessoa nasceu? '.format(pess)))
+    idade = atual  - nascimento
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+print('E tambem tivemos {} pessoas menores de idade'.format(totmenor))
