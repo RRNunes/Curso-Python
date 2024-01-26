5b7bb530f7d8c2c29d438936487f4d062f1bdb0f
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-26 01:53:51 +0000

    d55

--- Mundo 2/des55.py
+++ Mundo 2/des55.py
@@ -1,5 +1,8 @@
 """"FACA UM PROGRAMA QUE LEIA O PESO DE CINCO PESSOAS. NO FINAL, MOSTRE QUAL FOI O MAIOR E O MENOR PESO LIDOS."""
 
-peso = float(input('Digite o peso: '))
+maior = 0
+menor= 0
+for p in range(1, 6):
+    peso = float(input('Digite o peso: '))
 
 
