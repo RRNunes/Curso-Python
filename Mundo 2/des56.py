331d29ec1abdad2f4b8af339d94a4ee5d4c63082
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-26 18:39:45 +0000

    d56

--- Mundo 2/des56.py
+++ Mundo 2/des56.py
@@ -2,3 +2,12 @@
 A MEDIA DE IDADE DO GRUPO.
 QUAL O NOME DO HOMEM MAIS VELHO.
 QUANTAS MULHERES TEM MENOS DE 20 ANOS."""
+
+somaidade = 0
+for p in range(1, 5):
+    print(' {}pessoa'.format(p))
+    nome = str(input('Digite nome: ')).strip()
+    idade = int(input('Digite idade: '))
+    sexo = str(input('Digite sexo F/M : ')).strip()
+    somaidade += idade
+
