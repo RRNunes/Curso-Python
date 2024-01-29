47d361b59296bba9ba46ab927b9ee8c3ba22c5e0
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-29 22:18:04 +0000

    d62c

--- Mundo 2/des62.py
+++ Mundo 2/des62.py
@@ -8,8 +8,14 @@ primeiro = int(input('Divite termo: '))
 razao = int(input('Razao PA: '))
 termo = primeiro
 cont = 1
-while cont <10 :
-    print(' {} = '.format(termo), end=' ')
-    termo += razao
-    cont += 1
+mais = 10
+total = 0
+while mais != 0:
+    total = total + mais
+    while cont < total:
+        print(' {} = '.format(termo), end=' ')
+        termo += razao
+        cont += 1
+    print('PAUSA!!')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
 print('FIM!')
