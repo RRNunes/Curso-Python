c086faa277a04cef8e91a0bc2d8f591b9f195351
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-29 22:10:32 +0000

    d62

--- Mundo 2/des62.py
+++ Mundo 2/des62.py
@@ -1,3 +1,15 @@
 """"MELHORE O DESAFIO 61, PERGUNTANDO PARA O USUARIO SE ELE QUER MOSTRAR MAIS ALGUNS TERMOS . O PROGRAMA
 ENCERRA QUANDO ELE SISSER QUE QUER MOSTRAR O TERMO."""
 
+
+print('GERADOR DE PA')
+print('-='* 10)
+primeiro = int(input('Divite termo: '))
+razao = int(input('Razao PA: '))
+termo = primeiro
+cont = 1
+while cont <10 :
+    print(' {} = '.format(termo), end=' ')
+    termo += razao
+    cont += 1
+print('FIM!')
