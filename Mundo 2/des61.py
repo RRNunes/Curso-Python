7bc489ed798d26ccaadf4e6f125e655c6d85cdfa
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-29 21:48:14 +0000

    d61c

--- Mundo 2/des61.py
+++ Mundo 2/des61.py
@@ -1,3 +1,16 @@
 """"REFACA O DESAFIO 51, LENDO O PRIMEIRO TERMO E A RAZAO DE UMA PA, MOSTRANDO OS 10 PRIMEIROS TERMOS
 DA PROGERSSAO USADO A ESTRUTURA WHILE."""
 
+
+print('GERADOR DE PA')
+print('-=' *10)
+primeiro = int(input('Digite termo: '))
+razao = int(input('Razao DA PA: '))
+termo = primeiro
+cont = 1 
+while cont <= 10 :
+    print(' {} = '.format(termo), end=' ')
+    termo += razao
+    cont += 1
+print('FIM!')
+
