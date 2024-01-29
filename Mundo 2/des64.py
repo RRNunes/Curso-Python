31e468cb49be035e735dd87beb7231e5c08a7ebc
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-29 22:51:44 +0000

    d64c

--- Mundo 2/des64.py
+++ Mundo 2/des64.py
@@ -2,3 +2,10 @@
 QUE É A CONDICAO DE PARADA. NO FINAL, MOSTRE QUANTOS NUMEROS FORAM DIGITADOR E QUAL FOI A SOMA ENTRE ELES
 DESCONCIDERANDO O FLOG."""
 
+nun = cont = soma = 0
+num = int(input('Digite um numero ou [999 para parar]: '))
+while num != 999:
+    soma += num
+    cont += 1 
+    num = int(input('Digite um numero ou [999 para parar:]'))
+print('Voce digitou {} os valores somados sao {}'.format(cont, soma))
