35ba6be5015f5886d45ef30efe8b92529dc8813c
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-01-29 22:37:50 +0000

    d63c

--- Mundo 2/des63.py
+++ Mundo 2/des63.py
@@ -1,2 +1,20 @@
 """ESCREVA UM PROGRAMA QUE LEIA UM NUMERO N INTEIRO QUALQUER E MOSTRE NA TELA OS N PRIMEIROS ELEMENTOS DE UMA SEQUENCIA DE FIBONACCI
 EX: 0 - 1- 1 - 2 - 3 - 5 - 8"""
+
+print('-'*30)
+print('SEQUENCIA DE FIBONACCI')
+print('-'*30)
+n = int(input('Quantos termos voce quer mostrar? '))
+t1 = 0
+t2 = 1
+print('-'*30)
+print(' {} - {}'.format(t1, t2), end=' ')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(' - {}'.format(t3), end=' ')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' - FIM!')
+
