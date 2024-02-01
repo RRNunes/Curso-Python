31a1d2f65560249518d73351b8436942877eed44
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-01 22:56:15 +0000

    de65

--- Mundo 2/des65.py
+++ Mundo 2/des65.py
@@ -2,3 +2,17 @@
 ENTRE TODOS OS VALORES E QUAL FOI O MAIOR E MENOSR VALORE LIDOS. O PROGRAMA DEEVE PERGUNTAR AO USUARIO SE ELE QUER OU NAO
 CONTINUAR A DIGITAR VALORES"""
 
+menor = 0
+maior = 0
+media = 0
+num = 0
+inteiro = 0
+inteiro = int(input('Digite um numero ou 999 para finalizar: '))
+while num != 999:
+    inteiro = int(input('Digite um numero ou 999 para finalizar: '))
+    media = (inteiro + inteiro) % num
+    print('FIM!')
+print('Foram digitados {} numero, a media entre eles e {}'.format(num, media))
+
+
+
