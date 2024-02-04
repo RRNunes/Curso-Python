ef68549f6b0c700431e3b0c27fbc363d9deac3cb
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-04 01:21:37 +0000

    de67

--- Mundo 2/des66.py
+++ Mundo 2/des66.py
@@ -1,3 +1,12 @@
-"""Crie um programa que leia varios numeor inteiro pelo teclado.O programa so vai parar quando
-o usuario digitar o valor 999, que 'e sua condcao de parada. No final, mostre quantos numeros
-foram digitados e qual foi a soma entre eles(desconsiderando o flag)."""
+"""Crie um programa que leia varios numeros inteiro pelo teclado.O programa so vai parar quando
+o usuario digitar o valor 999, que e sua condicao de parada. No final, mostre quantos numeros
+foram digitados e qual foi a soma entre eles (desconsiderando o flag)."""
+
+soma = cont = 0
+while True:
+    numero = int(input('Digite um numero inteiro ou [999 PARA PARAR]:'))
+    if numero == 999:
+        break
+    cont += 1
+    soma += numero
+print(f'Voce digitou {cont} e a soma entre eles e {soma}')
--- Mundo 2/des67.py
+++ Mundo 2/des67.py
@@ -1,2 +1,13 @@
 """FACA UM PROGRAMA QUE MOSTRE A TABUADA DE VARIOS NUMEROS, UM DE CADA VEZ, PARA CADA VALOR GIGITADO
 PEELO USUARIO. O PROGRAMA SERA INTEROMPIDO QUANDO O  NUMERO SOLICITADO FOR NEGATIVO"""
+
+print('==== TABUADA =====')
+while True:
+    tabuada = int(input('Digite numero: '))
+    print('=' * 20)
+    if tabuada < 0:
+        break
+    for c in range(1, 11):
+        print(f'{tabuada} x {c:2} = {tabuada * c}')
+    print('=' * 20)
+print('PROGRAMA TABUADA ENVCERRANDO. VOLTE SEMORE!')
