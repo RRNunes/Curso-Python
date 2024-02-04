3984258b6dea9da95cefe6e4c25cb0b40a4bd61f
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-04 02:30:56 +0000

    d68

--- Mundo 2/des68.py
+++ Mundo 2/des68.py
@@ -6,7 +6,7 @@ from random import randint
 print('-=' * 20)
 print('VAMOS JOGAR PAR OU IMPAR?')
 print('-=' * 20)
-pontos = 0
+vezes = 0
 while True:
     jogador = int(input('Digite valor: '))
     computador = randint(0, 10)
@@ -14,28 +14,28 @@ while True:
     pi = ' '
     while pi not in 'PI':
         pi = str(input('PARAR ou IMPAR [P/I]? ')).strip().upper()[0]
-    print(f'Voce jogou {jogador}  o computador {computador}. Total de {total}', end=' ')
+    print(f'Voce jogou {jogador}  o computador {computador}. Total de {total}')
     if total % 2 == 0:
         print('DEU PAR!') 
-        rodada = 'p'
     else:
         print('DEU IMPAR!')
-        rodada = 'i'
     print('-=' * 20)
-    if pi == rodada:
-        print('Voce GANHOU...')
-        print('Vamos jogar novamente...')
-        print('-=' * 20)
-        pontos += 1
-    else:
-        print('Voce PERDEU!')
+    if pi == 'P':
+        if total % 2 == 0:
+            print('Voce GANHOU...')
+            vezes += 1
+        else:
+            print('Voce PERDEU!')
         break
+    elif pi == 'I':
+        if total % 2 == 1:
+            print('Voce VENCEU!')
+            vezes += 1
+        else:
+            print('Voce PERDEU!')
+            break
+    print('-=' * 20)
+    print('Vamos jogar novamente...')
 print('-=' * 20)
-print('GAME OVER!')
-if pontos == 0:
-    print('Voce nao ganhou nenhuma vez.')
-elif pontos == 1:
-    print('Voce ganhou somente 1 vez.')
-else:
-    print(f'Voce ganhou {pontos} vezes.')
+print(f'Voce ganhou {vezes} vezes.')
 
