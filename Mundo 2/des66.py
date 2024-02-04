0db2fbb20701eb7b3dab8c30885f5cb2c893c55a
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-04 00:46:49 +0000

    d71

---
+++ Mundo 2/des66.py
@@ -0,0 +1,3 @@
+"""Crie um programa que leia varios numeor inteiro pelo teclado.O programa so vai parar quando
+o usuario digitar o valor 999, que 'e sua condcao de parada. No final, mostre quantos numeros
+foram digitados e qual foi a soma entre eles(desconsiderando o flag)."""
