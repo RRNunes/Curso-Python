171fea71ae147d0d02c162d12c8e0bdf921cee34
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-05 22:11:00 +0000

    des77

---
+++ Mundo 3/des73.py
@@ -0,0 +1,9 @@
+""""crie uma tupla preenchida com os 20 primeiros colocador da tabela
+do campeonato brasileiro de futebol, na ordem de colocacao.
+depois mostre:
+
+a) OS 5 PRIMEIROS .
+B) OS ULTIMOS 4 COLOCADOS.
+C) TIMES EM ORDEM ALFABETICA.
+D) EM QUAL POSICAO ESTA O TIME DA CHAPECOENSE.
+"""
