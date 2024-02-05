0b95183f249b350372eb534879ab6262268090d1
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-05 21:53:22 +0000

    ex16

---
+++ Mundo 3/exe16.py
@@ -0,0 +1,18 @@
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim')
+#tuplas sao imutaveis
+#print(lanche[1]
+
+""""coloca em ordem"""
+#print(sorted(lanche))
+
+
+#for cont in range(0, len(lanche)):
+
+""""mostra a contagem"""
+#for pos, comida in enumerate(lanche):
+    
+for comida in lanche
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba')
+
+
