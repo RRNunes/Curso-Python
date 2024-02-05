dc14b8db81eadd750079971f8cc3ab020d683195
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-05 23:41:11 +0000

    de77c

--- Mundo 3/des77.py
+++ Mundo 3/des77.py
@@ -1,3 +1,16 @@
 """"CRIE UM PROGRAMA QUE TENHA UMA TUPLA COM VARIAS PALAVRAS(NAO USAR ACENTOS).
 DEPOIS DISSO,VOCE DEVE MOSTRAR, P-ARA CADA PALAVRA, QUAIS SAO AS SUAS VOGAIS.
 """
+
+palavras = ('aprender', 'programar', 'linguajem', 'python', 
+            'curso', 'gratis', 'estudar', 'praticar', 'trabalhar',
+            'mercado', 'programador', 'futuro')
+
+
+for p in palavras:
+    print(f'\n Na palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+
+
