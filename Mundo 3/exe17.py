d3d5c1be771705f088cc6b549376b6219e9d8299
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-07 01:45:54 +0000

    ex17

---
+++ Mundo 3/exe17.py
@@ -0,0 +1,25 @@
+""""LISTAS"""
+"""
+num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 0)
+num.pop(2)
+if 5 in num:
+    num.remove(5)
+else:
+    print('Nao existe esse valor')
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
+"""
+valores = []
+for cont in range(o, 5):
+    valores.append(int(input('Digite valor: ')))
+    
+#valores.append(5)
+#valores.append(9)
+#valores.append(4)
+for c, v in enumerate(valores):
+    print(f'Na posicao {c} encontrei o valor {v}')
+print('Cheguei ao final da lista')
