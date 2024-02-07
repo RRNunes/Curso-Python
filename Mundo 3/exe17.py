03381cc8a844f2b903c6339082cdae55f920de8c
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-07 01:59:26 +0000

    ex17c

--- Mundo 3/exe17.py
+++ Mundo 3/exe17.py
@@ -13,13 +13,17 @@ else:
 print(num)
 print(f'Essa lista tem {len(num)} elementos.')
 """
-valores = []
-for cont in range(o, 5):
-    valores.append(int(input('Digite valor: ')))
-    
+#valores = []
+#for cont in range(o, 5):
+#    valores.append(int(input('Digite valor: ')))
+
 #valores.append(5)
 #valores.append(9)
 #valores.append(4)
-for c, v in enumerate(valores):
-    print(f'Na posicao {c} encontrei o valor {v}')
-print('Cheguei ao final da lista')
+#for c, v in enumerate(valores):
+#    print(f'Na posicao {c} encontrei o valor {v}')
+#print('Cheguei ao final da lista')
+
+#for v in valores:
+#    print(f'{v}...', end=' ')
+
