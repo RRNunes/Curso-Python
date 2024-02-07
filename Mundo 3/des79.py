861a1cea0d68aa91399e65b282a27c7fdafea3eb
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-07 19:40:57 +0000

    des79c

--- Mundo 3/des79.py
+++ Mundo 3/des79.py
@@ -2,3 +2,17 @@
 CASO O NUMERO JA EXISTA LA DENTRO, ELE NAO SERA ADICIONADO. NO FINAL, SERAO EXIBIDOS TODOS OS 
 VALORES UNICOS DIGITADOS, EM ORDEM CRESCENTE."""
 
+numeros = list()
+while True:
+    n = int(input('Digite um valor : '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print(' Valor duplicado! Nao vou adicionar...')
+    r = str(input('Quer continumar [S/N]? '))
+    if r in 'Nn':
+            break
+print('-=' * 30)
+numeros.sort()
+print(f'Voce digitou os valores {numeros}')
