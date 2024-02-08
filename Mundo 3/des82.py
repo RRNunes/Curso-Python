67fc730b529dd79b6c398fb5e4cb6d32181753f1
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-08 20:08:17 +0000

    des82

--- Mundo 3/des82.py
+++ Mundo 3/des82.py
@@ -2,3 +2,20 @@
 CONTER APENAS OS VALORES PARES E VALORES IMPARES DIGITADOS, RESPECTIVAMENTE. AO FINAL, MOSTRE O CONTEUDO DAS
 TRES LISTAS GERADOS."""
 
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um numero: ')))
+    resp = str(input('Quer continuar [S/N]? '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        impares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
