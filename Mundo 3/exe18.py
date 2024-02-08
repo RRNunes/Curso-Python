7fb417e58511c7d0934119300184a585535b8991
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-08 23:25:08 +0000

    exe18

---
+++ Mundo 3/exe18.py
@@ -0,0 +1,13 @@
+"""teste = lista ()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
+"""
+
+galera = [['Joao', 19], ['Ana', 33], ['Joaquin', 13], ['Maria', 45]]
+print(galera[2][1])
