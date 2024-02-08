e78a23913c665e6022a2ff7d857991ba7112275b
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-08 20:14:00 +0000

    des83

--- Mundo 3/des83.py
+++ Mundo 3/des83.py
@@ -1,2 +1,19 @@
 """"CRIE UM PROGRAMA ONDE O USUARIO DIGITE UMA EXPRESSAO QUALQUER QUE USE PARENTESES. SEU APLICATIVO DEVERA
 ANALISAR SE A EXPRESSAO PASSADA ESTA COM OS PARENTESES ABERTOS E FECHADOS NA OR DEM CORRETA."""
+
+expr = str(input('Digite a expressao: '))
+pilha = []
+for simb in expr:
+    if simb == '()':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressao esta valida!')
+else:
+    print('Sua expressao esta errada!')
+    
