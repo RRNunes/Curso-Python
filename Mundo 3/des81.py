7bd378265a01df9581beb2d86c3d26c250b4f7e5
Katyanne R. R. Nunes <57458585+RRNunes@users.noreply.github.com>
2024-02-08 20:02:24 +0000

    des81

--- Mundo 3/des81.py
+++ Mundo 3/des81.py
@@ -4,3 +4,19 @@ DISSO, MOSTRE:
 A) QUANTOS NUMEROS FORAM DIGITADOS.
 B) A LISTA DE VALORES, ORDENADA DE FORMA DECRESCENTE.
 C) SE O VALOR 5 FOI DIGITADO E ESTA OU NAO NA LISTA."""
+
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar [S/N]? '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Voce digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores eem ordem decrescente sao {valores}.')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 nao foi encontrado na lista!')
+    
