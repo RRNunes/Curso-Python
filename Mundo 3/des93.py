8a5b2611a96da0bd5eb27c45e1e85bcdedbc58be
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-02-21 18:55:42 -0500

    des93

--- Mundo 3/des93.py
+++ Mundo 3/des93.py
@@ -4,3 +4,21 @@ CRIE UM PROGRAMA QUE GERENCIE O APROVEITAMENTO DE UM JOGADOR DE FUTEBOL. O PROGR
 E QUANTAS PARTIDADS ELE JOGOU. PEDOIS VAI LER A QUANTIDADE DE GOLS FEITOS EM CADA PARTIDA. NO FINAL, TUDO SERA GUARDADO EM UM DICIONARIO, INCLUINDO O TOTAL DE GOLS FEITOS DURANTE O CAMPEONATO.
 
 """
+
+jogador = dict()
+partidas = list()
+jogador['nome'] = str(input('Nome do Jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, tot):
+    partidas.append(int(input(f'  Quantos gols na partida {c}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partida.')
+for k, v in enumerate(jogador['gols']):
+    print(f'  => ')
