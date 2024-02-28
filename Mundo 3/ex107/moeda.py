5fb6be17d8b148a685443d2ea13bf30adcb59ab4
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-02-28 16:04:23 -0500

    xe108

--- Mundo 3/ex107/moeda.py
+++ Mundo 3/ex107/moeda.py
@@ -1,21 +1,29 @@
-"""CRIE UM MODULO CHAMADO moeda.py  QUE TENHA AS FUNCOES INCORPORADAS aumentar(), diminuir(),dobro() e metade().FACA TAMBEM UM PROGRAMA QUE importe ESSES MODULOS E USE ALGUMAS DESSAS FUNCOES."""
+"""EX: 107:
+CRIE UM MODULO CHAMADO moeda.py  QUE TENHA AS FUNCOES INCORPORADAS aumentar(), diminuir(),dobro() e metade().FACA TAMBEM UM PROGRAMA QUE importe ESSES MODULOS E USE ALGUMAS DESSAS FUNCOES.
 
-def aumentar(preco, taxa):
+EX 108: 
+Adapte o codigo do desafio 107, criando uma funcao adicional chamada moeda() que consiga mostrar os valores como um valor monetario formatado."""
+
+def aumentar(preco, taxa = 0):
     res = preco + (preco * taxa/100)
     return res
 
 
-def diminuir(preco, taxa):
+def diminuir(preco, taxa = 0):
     res = preco - (preco * taxa/100)
     return res
 
 
-def dobro(preco):
+def dobro(preco = 0):
     res = preco * 2
     return res
 
 
-def metade(preco):
+def metade(preco = 0):
     res = preco / 2
     return res
+
+
+def moeda(preco = 0, moeda ='R$'):
+    return f'{moeda}{preco:>8.2f}'.replace('.', ',')
 
--- Mundo 3/ex107/teste.py
+++ Mundo 3/ex107/teste.py
@@ -1,7 +1,7 @@
-from moeda import metade, dobro, aumentar
+import moeda 
 
 p = float(input('Digite o preco: R$ '))
-print(f'A metade de {p} e {metade(p)}')
-print(f'O dobro de R${p} e R${dobro(p)}')
-print(f'Aumentar 10%, temos R${aumentar(p, 10)}')
+print(f'A metade de {moeda.moeda(p)} e {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} e R${moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentar 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}')
 
