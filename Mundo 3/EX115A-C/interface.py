f2d5546d5441776e4245e623cc71585c0583aaed
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-02-28 19:40:36 -0500

    ex115a

---
+++ Mundo 3/EX115A-C/interface.py
@@ -0,0 +1,41 @@
+"""
+Exercício Python 115a:
+Vamos criar um menu em Python, usando modularização.
+
+"""
+
+def linha(tam = 42):
+    return '-' * tam
+
+
+
+def cabecalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
+    opc = leiaInt('\033[32mSua Opcao: \033[m') 
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('ERRO! por favor, digite um numero inteiro valido.')
+            continue
+        except (KeyboardInterrupt):
+            print('Entrada de dados foi interrompida pelo usuario.')
+            return 0
+        else:
+            return n
+
+
---
+++ Mundo 3/EX115A-C/sistema.py
@@ -0,0 +1,17 @@
+from interface import *
+from time import sleep
+
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('Opcao 1')
+    elif resposta == 2:
+        cabecalho('Opcao 2')
+    elif resposta == 3:
+        cabecalho('Sainado do sistema...Ate logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opcao invalida!\033[m')
+        sleep(2)
+
