a55ea859bc3a1779fc6babfd05a3e68d9dceb901
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-04-01 18:32:19 -0400

    ex01c

--- Exercicios/Desafios/des01.py
+++ Exercicios/Desafios/des01.py
@@ -1,3 +1,5 @@
-# Crie um script Python que leia o nome da pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+"""" Crie um script Python que leia o nome da pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado."""
+
+
 p = input('Qual é o seu nome?')
 print('Olá {}! Prazer em te conhecer!'.format(p))
